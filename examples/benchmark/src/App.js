4db5cd2ce9f51ecb0a2bd683f9ad6ff89b8663bc
mimecorg <mimec@mimec.org>
2024-06-02 10:28:31 +0200

    add benchmark for js-framework-benchmark

---
+++ examples/benchmark/src/App.js
@@ -0,0 +1,117 @@
+import { mutate, useState } from 'leaner';
+import { onMount } from 'leaner/web';
+
+const random = ( max ) => Math.round( Math.random() * 1000 ) % max;
+
+const A = [ 'pretty', 'large', 'big', 'small', 'tall', 'short', 'long', 'handsome', 'plain', 'quaint', 'clean',
+  'elegant', 'easy', 'angry', 'crazy', 'helpful', 'mushy', 'odd', 'unsightly', 'adorable', 'important', 'inexpensive',
+  'cheap', 'expensive', 'fancy' ];
+const C = [ 'red', 'yellow', 'blue', 'green', 'pink', 'brown', 'purple', 'brown', 'white', 'black', 'orange' ];
+const N = [ 'table', 'chair', 'house', 'bbq', 'desk', 'car', 'pony', 'cookie', 'sandwich', 'burger', 'pizza', 'mouse',
+  'keyboard'];
+
+let nextId = 1;
+
+const buildData = (count) => {
+  const data = new Array( count );
+
+  for ( let i = 0; i < count; i++ ) {
+    data[ i ] = {
+      id: nextId++,
+      label: `${A[ random( A.length ) ]} ${C[ random( C.length ) ]} ${N[ random( N.length ) ]}`,
+    };
+  }
+
+  return data;
+};
+
+function Button( props, children ) {
+  return [ 'div', { class: 'col-sm-6 smallpad' },
+    [ 'button', { type: 'button', class: 'btn btn-primary btn-block', ...props }, ...children ],
+  ];
+}
+
+function Row( { item, selected, select, remove } ) {
+  return [ 'tr', { class: { danger: () => selected() == item().id } },
+    [ 'td', { class: 'col-md-1' }, item.id ],
+    [ 'td', { class: 'col-md-4' }, [ 'a', { onclick: () => select( item().id ) }, item.label ] ],
+    [ 'td', { class: 'col-md-1' }, [ 'a', { onclick: () => remove( item().id ) },
+      [ 'span', { class: 'glyphicon glyphicon-remove', 'aria-hidden': 'true' } ],
+    ] ],
+    [ 'td', { class: 'col-md-6' } ],
+  ];
+}
+
+export function App() {
+  const [ data, setData ] = useState( [] );
+  const [ selected, setSelected ] = useState( 0 );
+
+  function run() {
+    setData( buildData( 1000 ) );
+    setSelected( 0 );
+  }
+
+  function runLots() {
+    setData( buildData( 10000 ) );
+    setSelected( 0 );
+  }
+
+  function add() {
+    setData( data => data.concat( buildData( 1000 ) ) );
+  }
+
+  function update() {
+    setData( mutate( data => {
+      for ( let i = 0; i < data.length; i += 10 )
+        data[ i ].label += ' !!!';
+    } ) );
+  }
+
+  function clear() {
+    setData( [] );
+    setSelected( 0 );
+  }
+
+  function swapRows() {
+    setData( mutate( data => {
+      if ( data.length > 998 ) {
+        const d1 = data[ 1 ];
+        const d998 = data[ 998 ];
+        data[ 1 ] = d998;
+        data[ 998 ] = d1;
+      }
+    } ) );
+  }
+
+  function select( id ) {
+    setSelected( id );
+  }
+
+  function remove( id ) {
+    setData( data => data.filter( item => item.id != id ) );
+  }
+
+  return [ 'div', { class: 'container' },
+    [ 'div', { class: 'jumbotron' },
+      [ 'div', { class: 'row' },
+        [ 'div', { class: 'col-md-6' }, [ 'h1', 'Leaner.js' ] ],
+        [ 'div', { class: 'col-md-6' },
+          [ 'div', { class: 'row' },
+            [ Button, { id: 'run', onclick: run }, 'Create 1,000 rows' ],
+            [ Button, { id: 'runlots', onclick: runLots }, 'Create 10,000 rows' ],
+            [ Button, { id: 'add', onclick: add }, 'Append 1,000 rows' ],
+            [ Button, { id: 'update', onclick: update }, 'Update every 10th row' ],
+            [ Button, { id: 'clear', onclick: clear }, 'Clear' ],
+            [ Button, { id: 'swaprows', onclick: swapRows }, 'Swap Rows' ],
+          ],
+        ],
+      ],
+    ],
+    [ 'table', { class: 'table table-hover table-striped test-data' },
+      [ 'tbody',
+        [ 'for', data, item => [ Row, { item, selected, select, remove } ] ],
+      ],
+    ],
+    [ 'span', { class: 'preloadicon glyphicon glyphicon-remove', 'aria-hidden': 'true' } ],
+  ];
+}
---
+++ examples/benchmark/src/main.js
@@ -0,0 +1,5 @@
+import { mount } from 'leaner/web';
+
+import { App } from './App.js';
+
+mount( App, document.querySelector( '#app' ) );
