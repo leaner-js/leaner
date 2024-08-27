2d9c726549c356945d0791b685d1b0de935464b2
mimecorg <mimec@mimec.org>
2024-08-27 20:38:12 +0200

    fix function return types

--- packages/leaner/types/web.d.ts
+++ packages/leaner/types/web.d.ts
@@ -5,12 +5,12 @@ interface App {
   use( callback: () => void ): void;
 }
 
-export declare function createApp( component: ( props: object, children: any[] ) => any ) : App;
+export declare function createApp( component: ( props: object, children: any[] ) => any ): App;
 
-export declare function inject( key: string | symbol ) : any;
+export declare function inject( key: string | symbol ): any;
 
-export declare function provide( key: string | symbol, value: any );
+export declare function provide( key: string | symbol, value: any ): void;
 
-export declare function onMount( callback: () => void );
+export declare function onMount( callback: () => void ): void;
 
-export declare function onDestroy( callback: () => void );
+export declare function onDestroy( callback: () => void ): void;
