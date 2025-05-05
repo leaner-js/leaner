38e6a7fe6d793ba1d3f9c08eab1a94f8417744c8
mimecorg <mimec@mimec.org>
2025-05-05 20:23:19 +0200

    use leaner-doc-utils to generate documentation

--- website/build/build.js
+++ website/build/build.js
@@ -1,20 +1,9 @@
-import { mkdir, readFile, stat, watch } from 'fs/promises';
-import { dirname, resolve } from 'path';
-import { join } from 'path/posix';
-import { fileURLToPath } from 'url';
-
-import { glob } from 'glob';
-import Handlebars from 'handlebars';
+import { buildDocs, inlineScript } from 'leaner-doc-utils';
 import { build, defineConfig } from 'vite';
-
-import { generate404, generateFile } from './utils/generate.js';
-import { createDevServer } from './utils/server.js';
-import { inlineScript } from './utils/vite-inline-script.js';
 
 import config from '../config.js';
 
-const __dirname = dirname( fileURLToPath( import.meta.url ) );
-const rootDir = dirname( __dirname );
+import leanerPackage from '../../packages/leaner/package.json' with { type: 'json' };
 
 let watch = false;
 
@@ -30,12 +19,6 @@ for ( let i = 2; i < process.argv.length; i++ ) {
   }
 }
 
-let template = null;
-let files = null;
-
-let pending = Promise.resolve();
-let queued = new Set();
-
 const viteConfig = defineConfig( {
   build: {
     watch,
@@ -48,112 +31,32 @@ const viteConfig = defineConfig( {
     },
   },
   plugins: [
+    buildDocs( {
+      ...config,
+      tags: { '$VERSION$': getCaretVersion( leanerPackage.version ) },
+      onStartup,
+    } ),
     inlineScript(),
   ],
 } );
 
-const version = await getVersion();
+const result = await build( viteConfig );
 
 if ( watch ) {
-  await runInWatchMode();
-} else {
-  await build( viteConfig );
-  await generateAllFiles();
-}
-
-async function generateAllFiles( server ) {
-  try {
-    const content = await readFile( resolve( rootDir, 'dist/index.html' ), 'utf-8' );
-
-    template = Handlebars.compile( content );
-
-    files = await glob( '**/*.md', { cwd: resolve( rootDir, 'docs' ), posix: true } );
-
-    for ( const file of files )
-      await generateFile( file, rootDir, template, config, version, files );
-
-    await generate404( '404.md', rootDir, template, config );
-
-    if ( server != null )
-      server.reload();
-  } catch ( err ) {
-    if ( err.code != 'ENOENT' )
-      console.error( err );
-  }
-}
-
-async function runInWatchMode() {
-  const distDir = resolve( rootDir, 'dist' );
-  await mkdir( distDir, { recursive: true } );
-
-  const port = config.port || 3000;
-
-  const server = await createDevServer( distDir, port );
-
-  console.log( `Listening on ${server.baseUrl}\n` );
-  console.log( 'Press q to exit\n' );
-
-  const bundle = await build( viteConfig );
-
-  bundle.on( 'event', event => {
-    if ( event.code == 'END' ) {
-      enqueue( () => generateAllFiles( server ) );
-    }
-  } );
-
-  const ac = new AbortController();
-  const watcher = watch( resolve( rootDir, 'docs' ), { recursive: true, signal: ac.signal } );
-
   process.stdin.setRawMode( true );
   process.stdin.setEncoding( 'utf-8' );
 
   process.stdin.on( 'data', ch => {
     if ( ch == 'q' || ch == '\x03' ) {
       process.stdin.pause();
-      ac.abort();
-      bundle.close();
-      server.close();
+      result.close();
     }
   } );
-
-  try {
-    for await ( const event of watcher ) {
-      if ( !queued.has( event.filename ) ) {
-        queued.add( event.filename );
-        enqueue( () => handleFileChange( event.filename, server ) );
-      }
-    }
-  } catch ( err ) {
-    if ( err.name == 'AbortError' )
-      return;
-    throw err;
-  }
 }
 
-async function handleFileChange( file, server ) {
-  if ( template == null )
-    return;
-
-  try {
-    const stats = await stat( resolve( rootDir, 'docs', file ) );
-    if ( stats.isFile() ) {
-      const inputFile = file.replaceAll( '\\', '/' );
-      const outputFile = inputFile.replace( /.md$/, '.html' );
-      console.log( join( 'dist', outputFile ) );
-      await generateFile( inputFile, rootDir, template, config, version, files );
-      queued.delete( file );
-      server.reload( outputFile );
-    }
-  } catch ( err ) {
-    if ( err.code != 'ENOENT' )
-      console.error( err );
-  }
-}
-
-function enqueue( fn ) {
-  pending.finally( () => {
-    pending = fn();
-  } );
+function onStartup( server ) {
+  console.log( `Listening on ${server.baseUrl}\n` );
+  console.log( 'Press q to exit\n' );
 }
 
 function help() {
@@ -162,14 +65,8 @@ function help() {
   process.exit( 1 );
 }
 
-async function getVersion() {
-  const path = resolve( __dirname, '../../packages/leaner/package.json' );
-  const body = await readFile( path, 'utf-8' );
-  const { version } = JSON.parse( body );
-
+function getCaretVersion( version ) {
   const parts = version.split( '.' );
-
   const firstNonZero = parts.findIndex( p => p != '0' );
-
   return parts.slice( 0, firstNonZero + 1 ).join( '.' );
 }
